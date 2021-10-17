67afa51a93e72903058d9703fc0e3892fb18eac6
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-10-17 14:38:54 -0500

    feat(alert_page): styling

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,43 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showAlert(context), 
+          child: const Text('Mostrar alerta'),
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder()
+          )
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: ( context ) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        title: const Text('Title'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text('Este es el contenido de la caja de la alerta'),
+            FlutterLogo(size: 100)
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context), 
+            child: const Text('Cancelar')
+          ),
+          TextButton(
+            onPressed: () {}, 
+            child: const Text('Aceptar')
+          )
+        ],
+      )
     );
   }
 }
