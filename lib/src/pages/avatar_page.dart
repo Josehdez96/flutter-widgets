bcb01d9bc9a06c51510ef14def9a65cbaf3fee9a
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2021-10-17 19:12:47 -0500

    feat(avatar_page): simple avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
@@ -13,11 +15,75 @@ class AvatarPage extends StatelessWidget {
             padding: EdgeInsets.only(right: 10),
             child: CircleAvatar(
             child: Text('JD'),
-            backgroundColor: Colors.brown,
-          ),
+              backgroundColor: Colors.brown,
+            ),
           )
         ],
       ),
+      body: const _CustomAnimatedContainer(),
     );
+  }
+}
+
+class _CustomAnimatedContainer extends StatefulWidget {
+  const _CustomAnimatedContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_CustomAnimatedContainer> createState() => _CustomAnimatedContainerState();
+}
+
+class _CustomAnimatedContainerState extends State<_CustomAnimatedContainer> {
+
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.pink;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(10);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Stack(
+      children: [
+        Center(
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.fastOutSlowIn,
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              borderRadius: _borderRadius,
+              color: _color
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 25,
+          right: 25,
+          child: FloatingActionButton(
+            onPressed: () => changeShape(),
+            child: const Icon(Icons.play_arrow),
+          )
+        )
+      ],
+    );
+  }
+
+  void changeShape() {
+
+    final random = Random();
+
+    setState(() {
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(400).toDouble();
+      _color = Color.fromRGBO(
+        random.nextInt(255), 
+        random.nextInt(255), 
+        random.nextInt(255), 
+        1
+      );
+      _borderRadius = BorderRadius.circular(random.nextInt(45).toDouble());
+    });
   }
 }
